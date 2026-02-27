a8158aedfd989e60e44be93647044fa1f1ce0624
Dimpash <mitya.p.76@gmail.com>
2026-02-27 11:04:59 +0300

    admin/

--- cv/urls.py
+++ cv/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('cv_index/<lang>', cv_index, name="cv-index"),
+    path('cv_index/<str:lang>/', cv_index, name="cv-index"),
     path('', cv_index, name="cv-start-0"),
-    path('<lang>', cv_start, name="cv-start"),
+    path('<str:lang>/', cv_start, name="cv-start"),
     path('portfolio_details/<lang>/<portfolio_name>', portfolio_details, name="portfolio-details"),
 ]
